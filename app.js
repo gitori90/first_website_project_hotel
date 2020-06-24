1084fb1420f6e582922acf17b5ff19702fcdb2a6
ori.f <orifreedman@gmail.com>
2020-06-24 16:44:49 +0300

    added a reserve page. the site now works by running the app.js in nodemon. node_modules folder has been included in gitignore because of unbelieveable amount of warnings.

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+// const date = require(__dirname + "/views/date.js");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+// var items = [];
+// var workItems = [];
+
+app.get("/", function(req, res){
+
+  // var currentDay = date.getDate();
+  // var daysList = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+  //
+  // let day = daysList[currentDay];
+
+  // if (currentDay === 6 || (currentDay === 0))
+  // {
+  //   day = "weekend";
+  //
+  // }
+  // else{
+  //   day = "weekday";
+  // }
+
+  // it renders the file list.ejs inside views folder!:
+  // res.render("list", {listTitle: day, newItems: items});
+
+  res.render("index");
+});
+
+app.get("/pricing", function(req, res){
+
+  res.render("pricing");
+});
+
+app.post("/", function(req, res){
+
+  // let item = req.body.newItem;
+  //
+  // if (req.body.list === "work"){
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // }
+  // else{
+  //
+  //   items.push(item);
+  //   res.redirect("/");
+  //
+  // }
+
+  // item = req.body.newItem;
+  // items.push(item);
+  // res.redirect("/");
+});
+
+// app.get("/work", function(req, res){
+//   res.render("list", {listTitle: "work list", newItems: workItems});
+// })
+// app.post("/work", function(req, res){
+//   let item = req.body.newItems;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
+//
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
+
+app.listen(3000, function(){
+  console.log("server started at 3000");
+});
